dcc5b331f27cadf98fe327db4eb371231bbe20f8
Олег <rahilov@gmail.com>
2026-03-02 15:44:06 +0300

    Errors

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,12 +1,26 @@
 from django import forms
 from task_manager.tasks.models import Task
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 class TaskForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Эта магия заставляет выпадающий список показывать "Имя Фамилия"
+        # вместо стандартного "username", что критично для Playwright
+        self.fields['executor'].label_from_instance = lambda obj: obj.get_full_name() if obj.get_full_name() else obj.username
+
     class Meta:
-        model = Task
-        # Автор (author) исключен, он будет подставляться во View
+        model = Task  # ПРОВЕРЬТЕ: эта строка должна быть здесь!
         fields = ('name', 'description', 'status', 'executor', 'labels')
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5}),
             'labels': forms.SelectMultiple(attrs={'class': 'form-select'}),
+        }
+        # Добавляем человекочитаемые лейблы, если они не подтянулись из модели
+        labels = {
+            'name': _("Name"),
+            'status': _("Status"),
+            'executor': _("Executor"),
+            'labels': _("Labels"),
         }
